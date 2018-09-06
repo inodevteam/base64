3cec8d9c7b5bf1ba957765072c1c79c116a89e43
Serega Zem <15526424+spark1991z@users.noreply.github.com>
2018-09-06 09:24:12 +0500

    Disable decode method

--- main.cpp
+++ main.cpp
@@ -5,62 +5,51 @@
 using namespace std;
 
 namespace crypto{
-    enum base64_method_t {
-      BASE64_ENCODE,
-      BASE64_DECODE
-    };
     class Base64{
       private:
-         static vector<uint8_t> _en;
+         static vector<uint8_t> _chars;
          static void init() {
-            if(_en.size()!=64){
+            if(_chars.size()!=64){
                for(uint8_t c='A';c<='Z';c++)
-                  _en.push_back(c);
+                  _chars.push_back(c);
                for(uint8_t c='a';c<='z';c++)
-                  _en.push_back(c);
+                  _chars.push_back(c);
                for(uint8_t c='0';c<='9';c++)
-                  _en.push_back(c);
-               _en.push_back('+');
-               _en.push_back('/');
+                  _chars.push_back(c);
+               _chars.push_back('+');
+               _chars.push_back('/');
             }
-            cout << "base64_init -> " << (_en.size()!=64 ? "fail" : "ok") << endl;
+            cout << "base64_init -> " << (_chars.size()!=64 ? "fail" : "ok") << endl;
          }
-         base64_method_t _method;
          uint8_t _tmp;
          size_t _mode;
          vector<uint8_t> _bufo;
-         Base64(const base64_method_t&_method) : _method(_method),_mode(0){
+         Base64() : _mode(0){
          }
       public:
-         static Base64 create(const base64_method_t&_method){
+         static Base64 create(){
             init();
-            return Base64(_method);
+            return Base64();
          }
          static const bool write(Base64&b64, const uint8_t&c){
             bool next = true;
-            switch(b64._method) {
-               case BASE64_ENCODE:
-                  switch(b64._mode){
-                     case 0:
-                        b64._bufo.push_back(_en[(c>>2)&0x3f]);
-                        b64._mode++;
-                        break;
-                     case 1:
-                        b64._bufo.push_back(_en[((b64._tmp<<4)+((c>>4)&0xf))&0x3f]);
-                        b64._mode++;
-                        break;
-                     case 2:
-                        next = false;
-                        b64._bufo.push_back(_en[((b64._tmp<<2)+((c>>6)&0x3))&0x3f]);
-                        b64._mode++;
-                        break;
-                     case 3:
-                        b64._bufo.push_back(_en[c&0x3f]);
-                        b64._mode=0;
-                        break;
-                  }
+            switch(b64._mode){
+               case 0:
+                  b64._bufo.push_back(_chars[(c>>2)&0x3f]);
+                  b64._mode++;
                   break;
-               case BASE64_DECODE:
+               case 1:
+                  b64._bufo.push_back(_chars[((b64._tmp<<4)+((c>>4)&0xf))&0x3f]);
+                  b64._mode++;
+                  break;
+               case 2:
+                  next = false;
+                  b64._bufo.push_back(_chars[((b64._tmp<<2)+((c>>6)&0x3))&0x3f]);
+                  b64._mode++;
+                  break;
+               case 3:
+                  b64._bufo.push_back(_chars[c&0x3f]);
+                  b64._mode=0;
                   break;
             }
             b64._tmp =c;
@@ -73,48 +62,42 @@ namespace crypto{
                b64._bufo.erase(b64._bufo.begin());
                return out;
             }
-            switch(b64._method) {
-               case BASE64_ENCODE:
-                  switch(b64._mode){
-                     case 0:
-                           out = '\0';
-                        break;
-                     case 1:
-                        if(b64._tmp!='\0'){
-                           out = _en[(b64._tmp<<4)&0x3f];
-                           b64._tmp = '\0';
-                        }
-                        b64._mode++;
-                        break;
-                     case 2:
-                        if(b64._tmp!='\0'){
-                           out = _en[(b64._tmp<<2)&0x3f];
-                           b64._tmp = '\0';
-                        }
-                        b64._mode++;
-                        break;
-                     case 3:
-                        if(b64._tmp!='\0'){
-                           out = _en[(b64._tmp)&0x3f];
-                           b64._tmp = '\0';
-                        }
-                        b64._mode=0;
-                        break;
-                     }
+            switch(b64._mode){
+               case 0:
+                     out = '\0';
                   break;
-               case BASE64_DECODE:
+               case 1:
+                  if(b64._tmp!='\0'){
+                     out = _chars[(b64._tmp<<4)&0x3f];
+                     b64._tmp = '\0';
+                  }
+                  b64._mode++;
                   break;
-            }
+               case 2:
+                  if(b64._tmp!='\0'){
+                     out = _chars[(b64._tmp<<2)&0x3f];
+                     b64._tmp = '\0';
+                  }
+                  b64._mode++;
+                  break;
+               case 3:
+                  if(b64._tmp!='\0'){
+                     out = _chars[(b64._tmp)&0x3f];
+                     b64._tmp = '\0';
+                  }
+                  b64._mode=0;
+                  break;
+               }
             return out;
          }
     };
-    vector<uint8_t> Base64::_en;
+    vector<uint8_t> Base64::_chars;
 }
 
 using namespace crypto;
 
 int main() {
-   Base64 b64e = Base64::create(BASE64_ENCODE);
+   Base64 b64e = Base64::create();
    string s = "Hello, world!";
    size_t x=0;
    uint8_t r;
