a152abc7f0d126103d7bf79929792eae385b3ade
spark1991z <spark1991z@gmail.com>
2018-12-11 11:44:18 +0500

    code optimization

--- base64.h
+++ base64.h
@@ -1,18 +1,16 @@
 #ifndef base64_h
     #define base64_h
+    #define BASE64_ENCODE 0x0
+    #define BASE64_DECODE 0x1
     #include "c_types.h"
-
-    #define BASE64_ENCODE 0
-    #define BASE64_DECODE 1
 
     typedef struct {
         uint8_t buf[2],buf_len,mode,tmp;
         int8_t method;
     } base64_ctx;
 
-    const bool base64_init(base64_ctx*,const uint8_t);
-    const bool base64_available(const base64_ctx*);
-    const bool base64_write(base64_ctx*,const uint8_t);
-    const uint8_t base64_read(base64_ctx*);
-
+    const bool base64_init (base64_ctx*,const uint8_t);
+    const bool base64_available (const base64_ctx*);
+    const bool base64_write (base64_ctx*,const uint8_t);
+    const uint8_t base64_read (base64_ctx*);
 #endif // base64_h
--- c_types.h
+++ c_types.h
@@ -1,39 +1,34 @@
 #ifndef c_types_h
-    #define c_types_h
+  #define c_types_h
+  // 8-bit
+  typedef unsigned char       uint8_t;
+  typedef signed char         sint8_t;
+  typedef signed char         int8_t;
+  // 16-bit
+  typedef unsigned short      uint16_t;
+  typedef signed short        sint16_t;
+  typedef signed short        int16_t;
+  // 32-bit
+  typedef unsigned long       uint32_t;
+  typedef signed long         sint32_t;
+  typedef signed long         int32_t;
+  // 64-bit
+  typedef signed long long    sint64_t;
+  typedef unsigned long long  uint64_t;
+  typedef unsigned long long  u_int64_t;
+  // real
+  typedef float               real32_t;
+  typedef double              real64_t;
+  // size
+  typedef unsigned int        size_t;
+  // nullptr
+  #ifndef NULL
+      #define NULL (void *)0
+  #endif /* NULL */
 
-    // 8-bit
-    typedef unsigned char       uint8_t;
-    typedef signed char         sint8_t;
-    typedef signed char         int8_t;
-    // 16-bit
-    typedef unsigned short      uint16_t;
-    typedef signed short        sint16_t;
-    typedef signed short        int16_t;
-    // 32-bit
-    typedef unsigned long       uint32_t;
-    typedef signed long         sint32_t;
-    typedef signed long         int32_t;
-    // 64-bit
-    typedef signed long long    sint64_t;
-    typedef unsigned long long  uint64_t;
-    typedef unsigned long long  u_int64_t;
-    // real
-    typedef float               real32_t;
-    typedef double              real64_t;
-
-    typedef unsigned int        size_t;
-
-    #ifndef NULL
-        #define NULL (void *)0
-    #endif /* NULL */
-
-    #ifndef __cplusplus
-        typedef unsigned char   bool;
-
-        #define true            (1)
-        #define false           (0)
-        #define TRUE            true
-        #define FALSE           false
-    #endif /* !__cplusplus */
-
+  #ifndef __cplusplus
+      typedef unsigned char   bool;
+      #define true            (1)
+      #define false           (0)
+  #endif /* !__cplusplus */
 #endif // c_types_h
